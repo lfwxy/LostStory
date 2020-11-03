863d837ff48f1aec70187756c08721e24202e655
lfwxy <acx6579215@163.com>
2020-11-03 21:58:31 +0800

    数据处理

---
+++ Assets/GameLogic/ScriptData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptData
+{
+    public int type;
+    public string pos;
+    public string name;
+    public string talk;
+    public string picName;
+
+    public ScriptData(int type, string pos, string name, string talk, string picName)
+    {
+        this.type = type;
+        this.pos = pos;
+        this.name = name;
+        this.talk = talk;
+        this.picName = picName;
+    }
+
+    public ScriptData(int type, string picName)
+    {
+        this.type = type;
+        this.picName = picName;
+
+    }
+}
+
